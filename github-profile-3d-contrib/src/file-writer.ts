736161bd5940a0fd8f3227529f9210b58e12f329
ArtieFalcon <sokolartemy@gmail.com>
2023-05-13 22:32:08 +0300

    just updated UI

--- github-profile-3d-contrib/src/file-writer.ts
+++
@@ -1,8 +0,0 @@
-import { mkdirSync, writeFileSync } from 'fs';
-
-export const OUTPUT_FOLDER = './profile-3d-contrib';
-
-export const writeFile = (fileName: string, content: string): void => {
-    mkdirSync(OUTPUT_FOLDER, { recursive: true });
-    writeFileSync(`${OUTPUT_FOLDER}/${fileName}`, content);
-};
